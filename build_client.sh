73fc516262ec06a3193164666249eed3eacf9ec4
sric <lzqiong@gmail.com>
2024-10-10 22:42:23 +0800

    build client locally

--- build_client.sh
+++ build_client.sh
@@ -1,1 +1,3 @@
-docker run --rm -v "$(pwd)/client":/app -it python:3.9.19-bookworm bash -c "cd /app && python -m pip install --upgrade pip setuptools wheel && python setup.py bdist_wheel"
+cd client
+python setup.py bdist_wheel
+cd ..
